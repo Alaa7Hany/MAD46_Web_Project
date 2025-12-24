8d57ad603124779d0064b2b76263b5bd7d230c79
Alaa7Hany <3laahanylol@gmail.com>
2025-12-24 22:51:35 +0200

    Implemented delete

--- Scripts/new_contact_script.js
+++ Scripts/new_contact_script.js
@@ -1,7 +1,29 @@
 $(document).on("pagecreate", "#contact-page", function () {
 
-    $("#submitBtn").on("tap", function (e) {
+    function getParameterByName(name) {
+        var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+    }
 
+    var currentId = getParameterByName("id");
+    var isEditMode = currentId != null;
+
+    if (isEditMode) {
+        $("h1").text("Edit Contact");
+
+        var contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+        var contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+        var contact = contacts.find(x => x.id == currentId);
+
+        if (contact) {
+            $("#name").val(contact.name);
+            $("#phone").val(contact.phone);
+            $("#email").val(contact.email);
+            $("#gender").val(contact.gender).slider("refresh");
+        }
+    }
+
+    $("#submitBtn").on("tap", function (e) {
         e.preventDefault();
 
         var fullName = $("#name").val().trim();
@@ -14,34 +36,35 @@ $(document).on("pagecreate", "#contact-page", function () {
             return;
         }
 
-        $.mobile.loading("show", {
-            text: "Saving...",
-            textVisible: true,
-            theme: "b",
-            html: ""
-        });
+        $.mobile.loading("show", { text: "Saving...", textVisible: true, theme: "b", html: "" });
+
+        var contacts = JSON.parse(localStorage.getItem("contacts")) || [];
+
+        if (isEditMode) {
+            var contactIndex = contacts.findIndex(x => x.id == currentId);
+            if (contactIndex > -1) {
+                contacts[contactIndex].name = fullName;
+                contacts[contactIndex].phone = phone;
+                contacts[contactIndex].email = email;
+                contacts[contactIndex].gender = gender;
+            }
+        } else {
+            var newContact = {
+                id: Date.now(),
+                name: fullName,
+                phone: phone,
+                email: email,
+                gender: gender,
+                img: "https://cdn-icons-png.flaticon.com/512/149/149071.png"
+            };
+            contacts.push(newContact);
+        }
+
+        localStorage.setItem("contacts", JSON.stringify(contacts));
 
         setTimeout(function () {
             $.mobile.loading("hide");
-        }, 1000);
-
-
-
-        let num = contacts.length;
-
-        var newContact = {
-            id: num + 1,
-            name: fullName,
-            phone: phone,
-            email: email,
-            gender: gender
-        };
-
-        contacts.push(newContact);
-
-        alert("Contact added successfully!");
-
-        $.mobile.changePage("index.html#list");
-
+            window.location.href = "index.html";
+        }, 500);
     });
 });
--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,45 +1,95 @@
-var contacts = [
-    {
-        id: 1,
-        name: "Mohamed Ali",
-        phone: "0100000",
-        email: "mohamed@mail.com",
-        img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-881954-2379004.jpg&fm=jpg",
-    },
-    {
-        id: 2,
-        name: "Alaa Hany",
-        phone: "0111111",
-        email: "alla@mail.com",
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&s",
-    },
-];
-let selectedId = null;
+function getContacts() {
+    var storedContacts = localStorage.getItem("contacts");
+    if (storedContacts) {
+        return JSON.parse(storedContacts);
+    } else {
+        var defaults = [
+            {
+                id: 1,
+                name: "Mohamed Ali",
+                phone: "0100000",
+                email: "mohamed@mail.com",
+                img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?cs=srgb&dl=pexels-italo-melo-881954-2379004.jpg&fm=jpg",
+            },
+            {
+                id: 2,
+                name: "Alaa Hany",
+                phone: "0111111",
+                email: "alla@mail.com",
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFU7U2h0umyF0P6E_yhTX45sGgPEQAbGaJ4g&s",
+            }
+        ];
+        localStorage.setItem("contacts", JSON.stringify(defaults));
+        return defaults;
+    }
+}
 
 function renderList() {
-    $("#contactList").empty();
-    contacts.forEach((c) => {
-        $("#contactList").append(`
-      <li>
-        <a href="#details" class="contact" data-id="${c.id}">
-          <img src="${c.img}" width="40">
-          <span>${c.name}</span>
-        </a>
-      </li>
-    `);
-    });
+    var contacts = getContacts();
+    var list = $("#contactList");
+
+    list.empty();
+
+    if (contacts.length === 0) {
+        list.append("<li>No contacts found.</li>");
+    } else {
+        contacts.forEach((c) => {
+            list.append(`
+                <li>
+                    <a href="#details" class="contact" data-id="${c.id}" data-transition="slide">
+                        <img src="${c.img}"> 
+                        <h2>${c.name}</h2>
+                        <p>${c.phone}</p>
+                    </a>
+                </li>
+            `);
+        });
+    }
+
+
+    try {
+        list.listview("refresh");
+    } catch (e) {
+        list.trigger("create");
+    }
 }
+
 
 $(document).on("pagebeforeshow", "#list", function () {
     renderList();
 });
 
-// When a contact is clicked, show the details
 $(document).on("click", ".contact", function () {
-    selectedId = $(this).data("id");
-    let c = contacts.find((x) => x.id === selectedId);
-    $("#detailsName").text(c.name);
-    $("#detailsPhone").text(c.phone);
-    $("#detailsEmail").text(c.email);
-    $("#detailsImg").attr("src", c.img);
+    var selectedId = $(this).data("id");
+    var contacts = getContacts();
+    var c = contacts.find((x) => x.id == selectedId);
+
+    if (c) {
+        $("#detailsName").text(c.name);
+        $("#detailsPhone").text(c.phone);
+        $("#detailsEmail").text(c.email);
+        $("#detailsImg").attr("src", c.img);
+    }
+    $("#editBtn").attr("href", "new_contact.html?id=" + c.id);
+    $("#deleteBtn").data("id", c.id);
+
+});
+
+$(document).on("click", "#deleteBtn", function (e) {
+    e.preventDefault();
+
+    var idToDelete = $(this).data("id");
+
+    if (confirm("Are you sure you want to delete this contact?")) {
+
+        var contacts = getContacts();
+
+
+        var newContacts = contacts.filter(c => c.id != idToDelete);
+
+        localStorage.setItem("contacts", JSON.stringify(newContacts));
+
+
+        window.location.href = "index.html";
+    }
 });
